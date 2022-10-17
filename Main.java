287ba8f4e44bf66d8a7622e8566922f379466012
Chae-space <90403366+Chae-space@users.noreply.github.com>
2022-10-17 23:54:53 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package ch1;
+
+import java.util.*;
+
+class Main {
+    public String solution(String str) {
+        String answer="";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(T.solution(str));
+
+    }
+}
+
+
+
+
