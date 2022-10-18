43846da26474b337d6f0a4502fde2b2f87171f59
Chae-space <90403366+Chae-space@users.noreply.github.com>
2022-10-18 23:58:47 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,20 @@ import java.util.*;
 
 class Main {
     public String solution(String str) {
-        String answer="";
 
+        char[] arr = str.toCharArray();
+        ArrayList<Character> list = new ArrayList<>(); //중복되지 않은 값만 넣어줄 list 생성
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!list.contains(arr[i])) { //ArrayList의 contains()메소드를 활용해 이미 존재하지 않는 값만 넣어준다
+                list.add(arr[i]);
+            }
+
+        }
+        String answer = "";
+        for (char x : list) {
+            answer += x;
+        }
         return answer;
     }
 
